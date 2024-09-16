6840fefbcfe59fb181f589a34a3b7744fe5b30d9
Daramola Kehinde <daramolakenny18@gmail.com>
2024-09-16 22:40:41 +0100

    second commit

--- script.js
+++ script.js
@@ -4,16 +4,33 @@
 
 var input1 = document.getElementById('contenta');
 var input2 = document.getElementById('contentb');
-var container = document.getElementById('container');
+var message = document.getElementById('errormessage');
+var inputValue = document.getElementById('resultmessage');
 
 function submitEvent(){
+    resultMessage();
+}
+
+
+function resultMessage(){
     var content1 = input1.value;
     var content2 = input2.value;
+    var output;
+    
+    if(content1 === '' && content2 === ''){
+        errorMessage();
+    } else{
+        output = content1.toUpperCase() + " " + 'Love' + " " + content2.toUpperCase() +  " " + Math.floor(Math.random() * 60 + 1) + '%';
+    }
+    var resultTag = document.createElement('p');
+    resultTag.textContent = output;
+    
+    inputValue.appendChild(resultTag);
+}
 
-    var output = content1 + " " + 'Love' + " " + content2 +  " " + Math.floor(Math.random() * 60 + 1) + '%';
+function errorMessage(){
+    var errorTag = document.createElement('p');
+    errorTag.textContent = "Please supply the names of the lovers."; 
 
-    var pTag = document.createElement('p');
-    pTag.textContent = output;
-
-    container.appendChild(pTag);
-}                                                                 
+    message.appendChild(errorTag); 
+}
